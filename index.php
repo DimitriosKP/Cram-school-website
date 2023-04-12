4f4b423d4825a8cb421488f3f0dede72694916c8
Dimitrios K. Peftitsis <104554941+DimitriosKP@users.noreply.github.com>
2023-04-12 12:54:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <div class="text-box">
             <h1>Welcome!</h1>
             <p> The cram-school is an online application that anyone can log in either as a student or as a teacher. 
-                If the user chooses to be a professor he will be able to add and delete announcements, work and have 
+                If the user chooses to be a teacher he will be able to add and delete announcements, work and have 
                 access to the student category.
             </p>
         </div>
